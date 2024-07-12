93b3b878bcbec58bf86bbc44a8c633cf8b55e049
Brian Neal <bneal@primehealthservices.com>
2024-07-12 12:06:07 -0500

    fix to see swagger

--- LearningAzure/Program.cs
+++ LearningAzure/Program.cs
@@ -4,14 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(opt => {
+	opt.AddPolicy("default", opt => {
+		opt.AllowAnyHeader();
+		opt.AllowAnyOrigin();
+		opt.AllowAnyMethod();
+	});
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
+
 	app.UseSwagger();
 	app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
